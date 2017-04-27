d66ed1d04fa3b478334ee1793bdbd5a97e785080
VTariko <vv.tariko@gmail.com>
2017-04-28 00:08:12 +0300

    Реализация математических операций с дробями

--- HW_VTariko_3/FractionsWork/Fraction.cs
+++ HW_VTariko_3/FractionsWork/Fraction.cs
@@ -7,8 +7,12 @@ using System.Threading.Tasks;
 
 namespace FractionsWork
 {
+	using static Math;
+
 	class Fraction
 	{
+		#region Свойства
+
 		/// <summary>
 		/// Числитель дроби
 		/// </summary>
@@ -19,34 +23,181 @@ namespace FractionsWork
 		/// </summary>
 		public int Denominator
 		{
-			get
-			{
-				return denominator;
-			}
+			get { return _denominator; }
 			set
 			{
 				if (value != 0)
 				{
-					denominator = value;
+					_denominator = value;
+				}
+				else
+				{
+					_denominator = 1;
 				}
 			}
 		}
 
-		private int numenator;
-		private int denominator;
+		#endregion
 
-		public Fraction(int numenator, int denominator)
+		#region Поля
+
+		private int _denominator;
+
+		#endregion
+
+		#region Конструкторы
+
+		/// <summary>
+		/// Констурктор по умолчанию
+		/// </summary>
+		public Fraction()
 		{
-			this.numenator = numenator;
-			if (denominator != 0)
-			{
-				this.denominator = denominator;
-			}
-			else
-			{
-				
-			}
-			
+			Numerator = 1;
+			Denominator = 1;
 		}
+
+		/// <summary>
+		/// Конструктор с передаваемыми параметрами числителя и знаменателя
+		/// </summary>
+		/// <param name="numerator">Числитель</param>
+		/// <param name="denominator">Знаменатель</param>
+		public Fraction(int numerator, int denominator)
+		{
+			Numerator = numerator;
+			Denominator = denominator;
+		}
+
+		#endregion
+
+		#region Методы
+
+		/// <summary>
+		/// Переопределение оператора сложения
+		/// </summary>
+		/// <param name="f1">Первое слагаемое</param>
+		/// <param name="f2">Второе слагаемое</param>
+		/// <returns></returns>
+		public static Fraction operator +(Fraction f1, Fraction f2)
+		{
+			int nok = Nok(f1.Denominator, f2.Denominator);
+			int numenator1 = f1.Numerator * nok / f1.Denominator;
+			int numenator2 = f2.Numerator * nok / f2.Denominator;
+
+			return new Fraction
+			{
+				Numerator = numenator1 + numenator2,
+				Denominator = nok
+			};
+		}
+
+		/// <summary>
+		/// Переопределение оператора разности
+		/// </summary>
+		/// <param name="f1">Уменьшаемое</param>
+		/// <param name="f2">Вычитаемое</param>
+		/// <returns></returns>
+		public static Fraction operator -(Fraction f1, Fraction f2)
+		{
+			int nok = Nok(f1.Denominator, f2.Denominator);
+			int numenator1 = f1.Numerator * nok / f1.Denominator;
+			int numenator2 = f2.Numerator * nok / f2.Denominator;
+
+			return new Fraction
+			{
+				Numerator = numenator1 - numenator2,
+				Denominator = nok
+			};
+		}
+
+		/// <summary>
+		/// Переопределение оператора умножения
+		/// </summary>
+		/// <param name="f1">Множимое</param>
+		/// <param name="f2">Множитель</param>
+		/// <returns></returns>
+		public static Fraction operator *(Fraction f1, Fraction f2)
+		{
+			Fraction fraction = new Fraction
+			{
+				Numerator = f1.Numerator * f2.Numerator,
+				Denominator = f1.Denominator * f2.Denominator
+			};
+			int nod = Nod(fraction.Numerator, fraction.Denominator);
+			fraction.Numerator /= nod;
+			fraction.Denominator /= nod;
+
+			return fraction;
+		}
+
+		/// <summary>
+		/// Переопределение оператора деления
+		/// </summary>
+		/// <param name="f1">Делимое</param>
+		/// <param name="f2">Делитель</param>
+		/// <returns></returns>
+		public static Fraction operator /(Fraction f1, Fraction f2)
+		{
+			Fraction fraction = new Fraction
+			{
+				Numerator = f1.Numerator * f2.Denominator,
+				Denominator = f1.Denominator * f2.Numerator
+			};
+			int nod = Nod(fraction.Numerator, fraction.Denominator);
+			fraction.Numerator /= nod;
+			fraction.Denominator /= nod;
+
+			return fraction;
+		}
+
+		/// <summary>
+		/// Наименьшее общее кратное
+		/// </summary>
+		/// <param name="a"></param>
+		/// <param name="b"></param>
+		/// <returns></returns>
+		private static int Nok(int a, int b)
+		{
+			return Abs(a * b) / Nod(a, b);
+		}
+
+		/// <summary>
+		/// Наибольший общий делитель
+		/// </summary>
+		/// <param name="a"></param>
+		/// <param name="b"></param>
+		/// <returns></returns>
+		private static int Nod(int a, int b)
+		{
+			while (b != 0)
+			{
+				int t = a % b;
+				a = b;
+				b = t;
+			}
+			return a;
+		}
+
+		/// <summary>
+		/// Поиск знака дроби
+		/// </summary>
+		/// <returns></returns>
+		private char Sign()
+		{
+			return (Numerator >= 0 && Denominator > 0) || (Numerator <= 0 && Denominator < 0) ? '\0' : '-';
+		}
+
+		/// <summary>
+		/// Переопределение метода ToString()
+		/// </summary>
+		/// <returns></returns>
+		public override string ToString()
+		{
+			//Если знаменатель равен единице - не отображаем его
+			string denom = Denominator != 1 ? string.Format($"/{Denominator}") : "";
+			//Если числитель равен нулю - вся дробь равна нулю
+			return Numerator == 0 ? "0" : string.Format("{0}{1}{2}", Sign(), Abs(Numerator), denom);
+		}
+
+		#endregion
 	}
 }
--- HW_VTariko_3/FractionsWork/FractionsWork.cs
+++ HW_VTariko_3/FractionsWork/FractionsWork.cs
@@ -1,4 +1,5 @@
-﻿using Helper;
+﻿using System;
+using Helper;
 
 namespace FractionsWork
 {
@@ -6,6 +7,27 @@ namespace FractionsWork
 	{
 		static void Main(string[] args)
 		{
+			Fraction fraction1 = new Fraction(4, 5);
+			Fraction fraction2 = new Fraction(3, 2);
+			
+			Console.WriteLine(fraction1);
+			Console.WriteLine(fraction2);
+
+			Fraction fraction;
+			const string res = "Результат {0} дробей {1} и {2}:\t{3}";
+
+
+			fraction = fraction1 + fraction2;
+			Console.WriteLine(res, "сложения", fraction1, fraction2, fraction);
+
+			fraction = fraction1 - fraction2;
+			Console.WriteLine(res, "вычитания", fraction1, fraction2, fraction);
+
+			fraction = fraction1 * fraction2;
+			Console.WriteLine(res, "умножения", fraction1, fraction2, fraction);
+
+			fraction = fraction1 / fraction2;
+			Console.WriteLine(res, "деления", fraction1, fraction2, fraction);
 
 			LogicHelper.Pause();
 		}
