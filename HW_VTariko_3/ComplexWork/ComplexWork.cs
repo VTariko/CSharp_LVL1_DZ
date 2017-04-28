4d0436554d772e74adf429301d680c4f56507a36
VTariko <vv.tariko@gmail.com>
2017-04-28 18:14:50 +0300

    Добавление комментариев + мелкий рефакторинг. Методы НОК и НОД вынесены в общий класс-помощник.

--- HW_VTariko_3/ComplexWork/ComplexWork.cs
+++ HW_VTariko_3/ComplexWork/ComplexWork.cs
@@ -7,15 +7,25 @@ namespace ComplexWork
 	{
 		static void Main(string[] args)
 		{
+			//Создадим два комплексных числа для демонстрации работы:
 			Complex com1 = new Complex(3, 4);
 			Complex com2 = new Complex(1, 2);
-			Console.WriteLine(com1);
-			Console.WriteLine(com2);
 
-			Console.WriteLine(com1.Plus(com2));
-			Console.WriteLine(com1.Minus(com2));
-			Console.WriteLine(com1.Multiply(com2));
-			Console.WriteLine(com1.Divide(com2));
+			//Выводим их на печать (благодаря переопределенному методу ToString() мы получим приятный глазу результат)
+			Console.WriteLine($"Первое комплексное число: {com1}");
+			Console.WriteLine($"Второе комплексное число: {com2}");
+
+			//Создаем шаблон сообщения для вывода на печать
+			const string res = "Результат {0} комплексных чисел {1} и {2}:\t{3}";
+
+			//Суммируем два комплексных числа:
+			Console.WriteLine(res, "сложения", com1, com2, com1.Plus(com2));
+			//Вычитаем два комплексных числа:
+			Console.WriteLine(res, "вычитания", com1, com2, com1.Minus(com2));
+			//Умножаем два комплексных числа:
+			Console.WriteLine(res, "умножения", com1, com2, com1.Multiply(com2));
+			//Делим два комплексных числа:
+			Console.WriteLine(res, "деления", com1, com2, com1.Divide(com2));
 
 			LogicHelper.Pause();
 		}
